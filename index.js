cd3048a82b20bd3cf12f8356513a4a7f2d12e8e7
inu255 <artmvhdv@mail.ru>
2020-10-15 21:51:13 +0300

    view object

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import Game from './src/game.js';
+import View from './src/view.js';
+
+const root = document.querySelector('#root');
 
 const game = new Game();
+const view = new View(root, 480, 640, 20, 10);
 
 window.game = game;
-
-console.log(game)
+window.view = view;
