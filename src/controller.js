c58b1b9fda841eeb47c9d96bfdfc7cffe54289ba
inu255 <artmvhdv@mail.ru>
2020-10-27 20:33:31 +0300

    pause feature added

--- src/controller.js
+++ src/controller.js
@@ -2,16 +2,59 @@ export default class Controller {
   constructor(game, view) {
     this.game = game;
     this.view = view;
+    this.isPlaying = false;
+    this.intervalId = nullж
 
     document.addEventListener('keydown', this.handleKeyDown.bind(this));
 
     this.view.renderStartScreen();
   }
 
+  update() {
+    this.game.movePieceDown();
+    updateView();
+  }
+
+  play() {
+    this.isPlaying = true;
+    this.startTimer();
+    updateView();
+  }
+
+  pause() {
+    this.isPlaying = false;
+    this.stopTimer();
+    updateView();
+  }
+
+  updateView() {
+    this.view.renderMainScreen(this.game.getState());
+  }
+
+  startTimer() {
+    if (!this.IntervalId) {
+      this.intervalId = setInterval(() => {
+        update();
+      }, 1000)
+    }
+  }
+
+  stopTimer() {
+    if (this.IntervalId) {
+      clearInterval(this.IntervalId);
+      this.intervalId = null;
+    }
+  }
+
   handleKeyDown(event) {
     switch (event.keyCode) {
       case 13: // ENTER
-        this.view.renderMainScreen(this.game.getState())
+      if (this.isPlaying) {
+        this.pause();
+      } else {
+        this.play();
+      }
+      break;
       case 37: // left
         this.game.movePieceLeft();
         this.view.renderMainScreen(this.game.getState());
